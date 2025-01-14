a12d9eb88cbe0a34ff3a3b2f661e7ea6beb35786
kawataki-yoshikazu <kawataki-yoshika@dmm.com>
2025-01-14 10:28:09 +0000

    fixed

--- src/c2c/__init__.py
+++ src/c2c/__init__.py
@@ -2,4 +2,4 @@ from .c2c import (GitignoreHandler, GitignoreRule, create_delimiter,
                   create_header, find_all_gitignores, is_binary_file,
                   scan_directory)
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
